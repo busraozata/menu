b97b10d0730fd0290c2152a5fa399a921b7368a3
Büşra Özata <busraozata1998@gmail.com>
2023-09-07 18:10:57 +0300

    home and menu

--- js/script.js
+++ js/script.js
@@ -1,6 +1,36 @@
 const scrollToTop = () => {
-	window.scroll({
-		top: 0,
-		behavior: "smooth"
-	});
+  window.scroll({
+    top: 0,
+    behavior: "smooth",
+  });
 };
+
+
+var swiper = new Swiper(".mySwiper", {
+  slidesPerView: 3,
+  spaceBetween: 10,
+
+  breakpoints: {
+    640: {
+      slidesPerView: 2,
+      spaceBetween: 20,
+    },
+    768: {
+      slidesPerView: 6,
+      spaceBetween: 20,
+    },
+    1024: {
+      slidesPerView: 7,
+      spaceBetween: 20,
+    },
+    1200: {
+      slidesPerView: 9,
+      spaceBetween: 40,
+    },
+    1600: {
+      slidesPerView: 12,
+      spaceBetween: 30,
+    },
+  },
+});
+

b0d4fbd90f9ff15446fd8e48444567c1dcdec619
Büşra Özata <busraozata1998@gmail.com>
2023-09-10 03:18:46 +0300

    revize

--- js/script.js
+++ js/script.js
@@ -9,7 +9,10 @@ const scrollToTop = () => {
 var swiper = new Swiper(".mySwiper", {
   slidesPerView: 3,
   spaceBetween: 10,
-
+  autoplay: {
+    delay: 2500,
+    disableOnInteraction: false,
+  },
   breakpoints: {
     640: {
       slidesPerView: 2,
@@ -33,4 +36,33 @@ var swiper = new Swiper(".mySwiper", {
     },
   },
 });
+$(document).ready(function () {
 
+  $(function () {
+    $('#startDate').datepicker({
+      format: 'dd/mm/yyyy'
+    });
+    $('#datetimepicker3').datetimepicker({
+      format: 'LT'
+    });
+    $('#datetimepicker3').datetimepicker({
+      format: 'LT'
+    });
+  });
+});
+$(document).ready(function() {
+  $('.minus').click(function () {
+    var $input = $(this).parent().find('input');
+    var count = parseInt($input.val()) - 1;
+    count = count < 1 ? 1 : count;
+    $input.val(count);
+    $input.change();
+    return false;
+  });
+  $('.plus').click(function () {
+    var $input = $(this).parent().find('input');
+    $input.val(parseInt($input.val()) + 1);
+    $input.change();
+    return false;
+  });
+});
